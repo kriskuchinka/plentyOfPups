334f7eae8b5603f617b73e382ed0adbbcd3558d4
Kris Kuchinka <kriskuchinka@gmail.com>
2023-08-06 11:56:49 -0700

    classtime changes

--- app/search/page.js
+++ app/search/page.js
@@ -1,7 +1,47 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function Search() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
+
+  const selectDog = () => {
+    setIsSearchClicked(!isSearchClicked);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/list/all");
+        const data = await response.json();
+        setData(data.message);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+  console.log(data);
   return (
     <>
-      <h1>Search, World</h1>
+      <h1>Search By Breed!</h1>
+      <h3>Breed Name:</h3>
+      <input
+        type="text"
+        name="breed_name"
+        placeholder="Type in a dog breed to begin..."
+        onClick={() => setIsSearchClicked(!isSearchClicked)}
+      />
+      <button>Search!</button>
+      {isSearchClicked &&
+        Object.keys(data).map((item) => (
+          <p onClick={() => selectDog()}>{item}</p>
+        ))}
     </>
   );
 }
